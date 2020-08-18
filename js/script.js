c27df243b43f23230c9fc3d23a3631442b861214
Lucas Alves <lcsjalves@gmail.com>
2020-08-18 09:51:07 -0300

    Minify script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,1 @@
-if (window.SimpleSlide) {
-
-	new SimpleSlide({
-		slide: "quote", // nome do atributo data-slide="principal"
-		time: 5000 // tempo de transição dos slides
-	});
-
-	new SimpleSlide({
-		slide: "portfolio",
-		time: 5000,
-		nav: true
-	});
-
-}
-
-if (window.SimpleAnime) {
-	new SimpleAnime();
-}
+window.SimpleSlide&&(new SimpleSlide({slide:"quote",time:5e3}),new SimpleSlide({slide:"portfolio",time:5e3,nav:!0})),window.SimpleAnime&&new SimpleAnime;
